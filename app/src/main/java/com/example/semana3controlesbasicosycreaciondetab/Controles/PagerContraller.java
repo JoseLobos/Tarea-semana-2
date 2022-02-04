9fe5711f7f88eba9dcbecab25a2e8daac944a0a3
Nathaly Milena Zelaya Caballero <88002638+milenazelaya@users.noreply.github.com>
2022-02-04 17:31:05 -0600

    codigo de clase PagerContraller

---
+++ app/src/main/java/com/example/semana3controlesbasicosycreaciondetab/Controles/PagerContraller.java
@@ -0,0 +1,4 @@
+package com.example.semana3controlesbasicosycreaciondetab.Controles;
+
+public class PagerContraller {
+}
--- app/src/main/java/com/example/semana3controlesbasicosycreaciondetab/Pantallatres.java
+++ app/src/main/java/com/example/semana3controlesbasicosycreaciondetab/Pantallatres.java
@@ -1,14 +1,34 @@
 package com.example.semana3controlesbasicosycreaciondetab;
 
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import com.example.semana3controlesbasicosycreaciondetab.Controles.PagerContraller;
+
+
 public class Pantallatres extends AppCompatActivity {
+
+    TabLayout tablayout;
+
+     ViewPager Viewuno;
+
+     TabItem Desayunosaludable,Almuerzosaludable,Cenasaludabe,Meriendasaludable;
+
+     PagerController pagerAdapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantallatres);
+
+
+
     }
 }
